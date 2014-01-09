cb25e48cebe94fa8a3c1beefe5f438c529f18b2f
ttitto <ttitto_prog@vfemail.net>
2014-01-09 14:51:25 -0800

    MovingLetters.cs solved to 66/100

--- CSharp/Exams/Exam2Evening140913/MovingLetters/MovingLetters.cs
+++ CSharp/Exams/Exam2Evening140913/MovingLetters/MovingLetters.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MovingLetters
 {
@@ -10,31 +8,47 @@ namespace MovingLetters
     {
         static void Main(string[] args)
         {
-            string[] words = Console.ReadLine().ToUpper().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            string[] words = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
             int wordsCnt = words.Count();
             int longest = words.Select(w => w.Length).Max();
-            for (int i = 0; i < wordsCnt; i++)
-            {
-                words[i] = words[i].PadLeft(longest, '*');
-            }
 
-            string letters = string.Empty;
+            string lettersStr = string.Empty;
 
             for (int ch = longest - 1; ch >= 0; ch--)
             {
                 for (int ii = 0; ii < wordsCnt; ii++)
                 {
-                    char last = words[ii][ch];
-                    if (last != '*')
+                    try
                     {
-                        letters += last;
+                        char last = words[ii].Last();
+                        words[ii] = words[ii].Remove(words[ii].Length - 1);
+                        lettersStr += last;
                     }
+                    catch (Exception)
+                    {
+                    }
+
                 }
             }
+            List<char> letters = lettersStr.ToList<char>();
+            int lettersCnt = letters.Count();
 
-            //Console.WriteLine(letters);
-
-
+            for (int pos = 0; pos < lettersCnt; pos++)
+            {
+                char current = letters[pos];
+                int targetIndx = (letters[pos].ToString().ToUpper()[0] - 64 + pos) % lettersCnt; // - target position
+                if (targetIndx > pos)
+                {
+                    letters.RemoveAt(pos);
+                    letters.Insert(targetIndx, current);
+                }
+                else
+                {
+                    letters.Insert(targetIndx, current);
+                    letters.RemoveAt(pos + 1);
+                }
+            }
+            Console.WriteLine(String.Join("", letters));
         }
     }
 }
