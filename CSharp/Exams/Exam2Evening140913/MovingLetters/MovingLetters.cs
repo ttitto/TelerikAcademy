22697931747d808ea1316003d44fd39267b52caa
ttitto <ttitto_prog@vfemail.net>
2014-01-08 21:13:49 -0800

    ExamPrep: Zerg solved

---
+++ CSharp/Exams/Exam2Evening140913/MovingLetters/MovingLetters.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovingLetters
+{
+    class MovingLettersClass
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().ToUpper().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            int wordsCnt = words.Count();
+            int longest = words.Select(w => w.Length).Max();
+            for (int i = 0; i < wordsCnt; i++)
+            {
+                words[i] = words[i].PadLeft(longest, '*');
+            }
+
+            string letters = string.Empty;
+
+            for (int ch = longest - 1; ch >= 0; ch--)
+            {
+                for (int ii = 0; ii < wordsCnt; ii++)
+                {
+                    char last = words[ii][ch];
+                    if (last != '*')
+                    {
+                        letters += last;
+                    }
+                }
+            }
+
+            //Console.WriteLine(letters);
+
+
+        }
+    }
+}
---
+++ CSharp/Exams/Exam2Evening140913/Zerg/Zerg.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace Zerg
+{
+    class ZergClass
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> alfabet = new Dictionary<string, int>() {
+            {"Rawr",0},
+            {"Rrrr",1},
+            {"Hsst",2},
+            {"Ssst",3},
+            {"Grrr",4},
+            {"Rarr",5},
+            {"Mrrr",6},
+            {"Psst",7},
+            {"Uaah",8},
+            {"Uaha",9},
+            {"Zzzz",10},
+            {"Bauu",11},
+            {"Djav",12},
+            {"Myau",13},
+            {"Gruh",14}
+            };
+            string[] strArr = Console.ReadLine().Split(new char[] { }, StringSplitOptions.RemoveEmptyEntries);
+            string input = String.Join("", strArr);
+            BigInteger multiplier = 1;
+            BigInteger sum = 0;
+            for (int i = input.Length - 4; i >= 0; i -= 4)
+            {
+                int codedNum;
+                if (alfabet.TryGetValue(input.Substring(i, 4), out codedNum))
+                {
+                    sum += codedNum * multiplier;
+                    multiplier *= 15;
+                }
+            }
+            Console.WriteLine("{0}", sum);
+        }
+    }
+}
