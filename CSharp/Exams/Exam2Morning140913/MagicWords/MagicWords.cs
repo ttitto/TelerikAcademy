fa94f6824fb80dc474a7751b976ff14bcf0ccca9
ttitto <ttitto_prog@vfemail.net>
2014-01-17 06:51:24 -0800

    MultiverseCommunication solved 100/100 MagicWords started

---
+++ CSharp/Exams/Exam2Morning140913/MagicWords/MagicWords.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicWords
+{
+    class MagicWordsClass
+    {
+        static int n = 0;
+        static void Main(string[] args)
+        {
+            n = int.Parse(Console.ReadLine());
+            List<string> arr = new List<string>(n);
+            for (int ii = 0; ii < n; ii++)
+            {
+                arr.Add(Console.ReadLine());
+            }
+
+            Reorder(arr);
+            Print(arr);
+        }
+        private static void Reorder(List<string> arr)
+        {
+            int indx = 0;
+            for (int i = 0; i < n; i++)
+            {
+                indx = arr[i].Length % (arr.IndexOf(arr[i])+2);
+
+                arr.Insert(indx, arr[i]);
+            }
+        }
+        private static void Print(List<string> arr)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
---
+++ CSharp/Exams/Exam2Morning140913/MultiverseCommunication/MultiverseCommunication.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MultiverseCommunication
+{
+    class MultiverseCommunicationClass
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Regex rgx = new Regex(@"CHU|TEL|OFT|IVA|EMY|VNB|POQ|ERI|CAD|K-A|IIA|YLO|PLA");
+            string[] arr = new string[] { "CHU", "TEL", "OFT", "IVA", "EMY", "VNB", "POQ", "ERI", "CAD", "K-A", "IIA", "YLO", "PLA" };
+
+            MatchCollection matches = rgx.Matches(input);
+            BigInteger multiplier = 1;
+            BigInteger sum = 0;
+
+            for (int i = matches.Count - 1; i >= 0; i--)
+            {
+                sum += Array.IndexOf(arr, matches[i].ToString()) * multiplier;
+                multiplier *= 13;
+            }
+            Console.WriteLine("{0}", sum);
+        }
+    }
+}
+
+
