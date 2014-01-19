6ae1cbf1812d9576fe80101b14e5be10c284b125
ttitto <ttitto_prog@vfemail.net>
2014-01-19 12:39:57 -0800

    DecodeAndDecrypt started

---
+++ CSharp/Exams/Exam2Morning140913/DecodeAndDecrypt/DecodeAndDecrypt.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecodeAndDecrypt
+{
+    class DecodeAndDecryptClass
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string message;
+            string cypher;
+            Decode(input);
+        }
+
+        private static void Decode(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
