5727ffb5fc576ce1c28a020471e0db3fb37d4ed0
ttitto <ttitto_prog@vfemail.net>
2014-02-13 12:24:44 -0800

    ExtensionsLambdaDelegates2014HW solved

---
+++ CSharpOOP/Homeworks/ExtensionsLambdaDelegates2014HW/ConvergentSeries/ConvergentSeries.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvergentSeries
+{
+    class ConvergentSeriesClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0:0.00}", CalculateSerie(indx => 1 / Math.Pow(2, indx), 0.01));
+            Console.WriteLine("{0:0.00}", CalculateSerie(indx => 1 / Factorial(indx), 0.01));
+            Console.WriteLine("{0:0.00}", CalculateSerie(indx => Math.Pow(-1,indx+1)*1 / Math.Pow(2, indx), 0.01));
+        }
+        static double Factorial(int num)
+        {
+            double result = 1;
+            if (num == 1 || num == 0) return 1;
+            for (int i = num; i > 0; i--)
+            {
+                result *= i;
+            }
+            return result;
+        }
+        static double CalculateSerie(Func<int, double> function, double precision)
+        {
+            int cnt = 1;
+            double previousSum;
+            double sum = 0;
+            do
+            {
+                previousSum = sum;
+                sum += function(cnt);
+                cnt++;
+            } while (Math.Abs(sum - previousSum) > precision);
+
+            return sum+1;
+        }
+    }
+}
---
+++ CSharpOOP/Homeworks/ExtensionsLambdaDelegates2014HW/LongestString/LongestString.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestString
+{
+    /*17. Write a program to return the string with maximum length from an array of strings. Use LINQ.*/
+    class LongestStringClass
+    {
+        static Random rnd = new Random();
+        static void Main(string[] args)
+        {
+            string[] strArr = new string[rnd.Next(10, 30)];
+            for (int i = 0; i < strArr.Length; i++)
+            {
+                strArr[i] = new string((char)rnd.Next(0, 255), rnd.Next(2, 20));
+            }
+
+            var longest = from str in strArr
+                          orderby str.Length descending
+                          select str;
+
+            Console.WriteLine(longest.ToList()[0]);
+
+        }
+    }
+}
