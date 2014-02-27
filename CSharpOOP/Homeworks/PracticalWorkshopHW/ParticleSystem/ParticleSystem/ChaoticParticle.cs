790da0565abc021b592457a00f01a5deeedf34c7
ttitto <ttitto_prog@vfemail.net>
2014-02-27 13:18:00 -0800

    ChaoticParticle.cs created and tested

---
+++ CSharpOOP/Homeworks/PracticalWorkshopHW/ParticleSystem/ParticleSystem/ChaoticParticle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParticleSystem
+{
+    class ChaoticParticle : Particle
+    {
+        public ChaoticParticle(MatrixCoords position, MatrixCoords speed)
+            : base(position, speed)
+        { }
+        private Random rnd = new Random();
+        protected override void Move()
+        {
+            base.Move();
+            this.Accelerate(new MatrixCoords(rnd.Next(-1, 2), rnd.Next(-1, 2)));
+        }
+        public override char[,] GetImage()
+        {
+            return new char[,] { { '#' } };
+        }
+
+    }
+}
