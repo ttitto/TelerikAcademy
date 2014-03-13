6c257effb9b0284be41de73ad5206a759fee0616
ttitto <ttitto@users.noreply.github.com>
2014-03-13 14:48:59 +0200

    < sign direction changed

--- CSharp/Homeworks/OperatorsExpressionsHW/Ex07PrimeNumbers/Ex07PrimeNumbers.cs
+++ CSharp/Homeworks/OperatorsExpressionsHW/Ex07PrimeNumbers/Ex07PrimeNumbers.cs
@@ -12,15 +12,15 @@ namespace Ex07PrimeNumbers
         static void Main(string[] args)
         {
             uint myInt = uint.Parse(Console.ReadLine());
-            bool isPrime = (myInt >= 2 ? true : myInt % 2 != 0) &&
-                (myInt >= 3 ? true : myInt % 3 != 0) &&
-                (myInt >= 4 ? true : myInt % 4 != 0) &&
-                (myInt >= 5 ? true : myInt % 5 != 0) &&
-                (myInt >= 6 ? true : myInt % 6 != 0) &&
-                (myInt >= 7 ? true : myInt % 7 != 0) &&
-                (myInt >= 8 ? true : myInt % 8 != 0) &&
-                (myInt >= 9 ? true : myInt % 9 != 0) &&
-                (myInt >= 10 ? true : myInt % 10 != 0);
+            bool isPrime = (myInt <= 2 ? true : myInt % 2 != 0) &&
+                (myInt <= 3 ? true : myInt % 3 != 0) &&
+                (myInt <= 4 ? true : myInt % 4 != 0) &&
+                (myInt <= 5 ? true : myInt % 5 != 0) &&
+                (myInt <= 6 ? true : myInt % 6 != 0) &&
+                (myInt <= 7 ? true : myInt % 7 != 0) &&
+                (myInt <= 8 ? true : myInt % 8 != 0) &&
+                (myInt <= 9 ? true : myInt % 9 != 0) &&
+                (myInt <= 10 ? true : myInt % 10 != 0);
 
             Console.WriteLine(isPrime);
         }
