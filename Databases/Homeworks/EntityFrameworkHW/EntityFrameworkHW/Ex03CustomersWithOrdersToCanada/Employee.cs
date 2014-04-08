75bf16790165ba0fc154a9dc9b44d75c07ecb81a
ttitto <ttitto@users.noreply.github.com>
2014-04-08 15:49:42 +0300

    Task 9 started

--- Databases/Homeworks/EntityFrameworkHW/EntityFrameworkHW/Ex03CustomersWithOrdersToCanada/Employee.cs
+++ Databases/Homeworks/EntityFrameworkHW/EntityFrameworkHW/Ex03CustomersWithOrdersToCanada/Employee.cs
@@ -1,16 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity.Core.Metadata.Edm;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using EntityFrameworkDemo.Data;
-
-namespace Ex03CustomersWithOrdersToCanada
+﻿namespace Ex03CustomersWithOrdersToCanada
 {/*Ex08: By inheriting the Employee entity class create a class which allows employees to access their corresponding territories as property of type EntitySet<T>.
 */
+    //using EntityFrameworkDemo.Data;
+    //using System.Data.Linq;
     //public partial class Employee
     //{
-    //   public  EntitySet<Employee>{get;set;}
+    //    private EntitySet<Territory> entityTerritories;
+    //    public EntitySet<Territory> EntityTerritories
+    //    {
+    //        get
+    //        {
+    //            using (var dbContext = new NorthwindEntities())
+    //            {
+    //                return this.entityTerritories;
+    //            }
+    //        }
+
+    //    }
     //}
 }
--- Databases/Homeworks/EntityFrameworkHW/EntityFrameworkHW/Ex03CustomersWithOrdersToCanada/Program.cs
+++ Databases/Homeworks/EntityFrameworkHW/EntityFrameworkHW/Ex03CustomersWithOrdersToCanada/Program.cs
@@ -6,6 +6,7 @@
     using System.Text;
     using System.Threading.Tasks;
     using EntityFrameworkDemo.Data;
+    using System.Data.Common;
     class Program
     {
         static void Main()
@@ -18,7 +19,7 @@
             {
                 Console.WriteLine(customer.CustomerID);
             }
-            foreach (var order in SalesByPeriodAndDestination("Canada", new DateTime(1997, 01, 01), new DateTime(1997, 12, 31))) 
+            foreach (var order in SalesByPeriodAndDestination("Canada", new DateTime(1997, 01, 01), new DateTime(1997, 12, 31)))
             {
                 Console.WriteLine(order.OrderID);
             }
@@ -70,6 +71,57 @@ WHERE YEAR(o.ShippedDate)={0} AND o.ShipCountry={1}";
                 var myOrders = dbContext.Orders.Where(o => o.ShipCountry == region && o.ShippedDate <= endDate && o.ShippedDate >= startDate).Select(o => o);
                 return myOrders.ToList();
             }
+        }
+
+        /*Ex09: Create a method that places a new order in the Northwind database. The order should contain several 
+         order items. Use transaction to ensure the data consistency.*/
+        //http://www.devart.com/dotconnect/mysql/docs/transactionsef.html
+        public bool AddOrder()
+        {
+            try
+            {
+
+                using (var dbContext = new NorthwindEntities())
+                {
+                    dbContext.Database.Connection.Open();
+                    using (DbTransaction tran = dbContext.Database.Connection.BeginTransaction())
+                    {
+                        Order order = SplitOrder("VINET", 5, new DateTime(2014, 05, 04), new DateTime(2014, 05, 04), new DateTime(2014, 05, 04), 2, 3.2500m, "Blondel père et fils", "Åkergatan 24", "Bräcke", "NM", "87110", "USA");
+
+                        dbContext.Orders.Add(order);
+                        dbContext.SaveChanges();
+
+                        return true;
+                    }
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+
+                //Rollback transaction
+               
+            }
+        }
+
+        private Order SplitOrder(string customerID = null, int employeeID = 0, DateTime orderDate = default(DateTime), DateTime requiredDate = default(DateTime), DateTime shippedDate = default(DateTime), int shipVia = 0, decimal freight = 0m, string shipName = null, string shipAddress = null, string shipCity = null, string shipRegion = null, string shipPostalCode = null, string shipCountry = null)
+        {
+            return new Order
+            {
+                CustomerID = customerID,
+                EmployeeID = employeeID,
+                OrderDate = orderDate,
+                RequiredDate = requiredDate,
+                ShippedDate = shippedDate,
+                ShipVia = shipVia,
+                Freight = freight,
+                ShipName = shipName,
+                ShipAddress = shipAddress,
+                ShipCity = shipCity,
+                ShipRegion = shipRegion,
+                ShipPostalCode = shipPostalCode,
+                ShipCountry = shipCountry
+            };
         }
     }
 }
