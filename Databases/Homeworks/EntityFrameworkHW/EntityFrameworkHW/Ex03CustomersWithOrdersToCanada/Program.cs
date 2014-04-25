1fff77758f7025f1fb41fff08f746f660ff1e55e
ttitto <ttitto@users.noreply.github.com>
2014-04-25 15:45:50 +0300

    todo: {"SqlConnection does not support parallel transactions."}

--- Databases/Homeworks/EntityFrameworkHW/EntityFrameworkHW/Ex03CustomersWithOrdersToCanada/Program.cs
+++ Databases/Homeworks/EntityFrameworkHW/EntityFrameworkHW/Ex03CustomersWithOrdersToCanada/Program.cs
@@ -11,19 +11,20 @@
     {
         static void Main()
         {
-            foreach (var customer in FindCustomersWithOrdersToDestination(1997, "Canada"))
-            {
-                Console.WriteLine(customer.CustomerID);
-            }
-            foreach (var customer in FindCustomersWithOrdersToDestinationNative(1997, "Canada"))
-            {
-                Console.WriteLine(customer.CustomerID);
-            }
-            foreach (var order in SalesByPeriodAndDestination("Canada", new DateTime(1997, 01, 01), new DateTime(1997, 12, 31)))
-            {
-                Console.WriteLine(order.OrderID);
-            }
+            //foreach (var customer in FindCustomersWithOrdersToDestination(1997, "Canada"))
+            //{
+            //    Console.WriteLine(customer.CustomerID);
+            //}
+            //foreach (var customer in FindCustomersWithOrdersToDestinationNative(1997, "Canada"))
+            //{
+            //    Console.WriteLine(customer.CustomerID);
+            //}
+            //foreach (var order in SalesByPeriodAndDestination("Canada", new DateTime(1997, 01, 01), new DateTime(1997, 12, 31)))
+            //{
+            //    Console.WriteLine(order.OrderID);
+            //}
 
+            Console.WriteLine(AddOrder());
         }
         /*Ex03: Write a method that finds all customers who have orders made in 1997 and shipped to Canada.*/
         public static IEnumerable<Customer> FindCustomersWithOrdersToDestination(int year, string destination)
@@ -76,35 +77,44 @@ WHERE YEAR(o.ShippedDate)={0} AND o.ShipCountry={1}";
         /*Ex09: Create a method that places a new order in the Northwind database. The order should contain several 
          order items. Use transaction to ensure the data consistency.*/
         //http://www.devart.com/dotconnect/mysql/docs/transactionsef.html
-        public bool AddOrder()
+        public static bool AddOrder()
         {
-            try
+            using (var dbContext = new NorthwindEntities())
             {
-
-                using (var dbContext = new NorthwindEntities())
+                dbContext.Database.Connection.Open();
+                using (DbTransaction tran = dbContext.Database.Connection.BeginTransaction())
                 {
-                    dbContext.Database.Connection.Open();
-                    using (DbTransaction tran = dbContext.Database.Connection.BeginTransaction())
+                    Order order = SplitOrder("VINUT", 5, new DateTime(2014, 05, 04), new DateTime(2014, 05, 04), new DateTime(2014, 05, 04), 2, 3.2500m, "Blondel père et fils", "Åkergatan 24", "Bräcke", "NM", "87110", "USA");
+
+                    dbContext.Orders.Add(order);
+                    dbContext.SaveChanges();
+                    //if order inserted successfully in the DB, continue with orderdetails
+                    if (dbContext.SaveChanges() > 0)
                     {
-                        Order order = SplitOrder("VINET", 5, new DateTime(2014, 05, 04), new DateTime(2014, 05, 04), new DateTime(2014, 05, 04), 2, 3.2500m, "Blondel père et fils", "Åkergatan 24", "Bräcke", "NM", "87110", "USA");
-
-                        dbContext.Orders.Add(order);
-                        dbContext.SaveChanges();
-
-                        return true;
+                        if (AddOrderDetails(order.OrderID) == null)
+                        {
+                            tran.Rollback();
+                            return false;
+                        }
+                        else
+                        {
+                            tran.Commit();
+                            return true;
+                        }
                     }
-                }
-                return true;
-            }
-            catch (Exception)
-            {
+                    //else Rollback the transaction
+                    else
+                    {
+                        tran.Rollback();
+                        return false;
+                    }
 
-                //Rollback transaction
-               
+                }
+
             }
         }
 
-        private Order SplitOrder(string customerID = null, int employeeID = 0, DateTime orderDate = default(DateTime), DateTime requiredDate = default(DateTime), DateTime shippedDate = default(DateTime), int shipVia = 0, decimal freight = 0m, string shipName = null, string shipAddress = null, string shipCity = null, string shipRegion = null, string shipPostalCode = null, string shipCountry = null)
+        private static Order SplitOrder(string customerID = null, int employeeID = 0, DateTime orderDate = default(DateTime), DateTime requiredDate = default(DateTime), DateTime shippedDate = default(DateTime), int shipVia = 0, decimal freight = 0m, string shipName = null, string shipAddress = null, string shipCity = null, string shipRegion = null, string shipPostalCode = null, string shipCountry = null)
         {
             return new Order
             {
@@ -122,6 +132,40 @@ WHERE YEAR(o.ShippedDate)={0} AND o.ShipCountry={1}";
                 ShipPostalCode = shipPostalCode,
                 ShipCountry = shipCountry
             };
+        }
+
+        private static List<Order_Detail> AddOrderDetails(int orderId)
+        {
+            List<Order_Detail> orderDetails = new List<Order_Detail>();
+            Console.WriteLine("Insert each order detail in a single row with values separated by \", \" or \n add \"end\" to finish");
+            string cmd = Console.ReadLine();
+            while (cmd != "end")
+            {
+                string[] singleOrderDetail = Console.ReadLine().Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries);
+                //If string doesnt have all necessary values then return null
+                if (singleOrderDetail.Count() != 4)
+                {
+                    return null;
+                }
+                Order_Detail detail = new Order_Detail
+                {
+                    OrderID = orderId,
+                    ProductID = int.Parse(singleOrderDetail[0]),
+                    UnitPrice = decimal.Parse(singleOrderDetail[1]),
+                    Quantity = short.Parse(singleOrderDetail[2]),
+                    Discount = float.Parse(singleOrderDetail[3])
+                };
+                orderDetails.Add(detail);
+
+            }
+            if (orderDetails.Count() < 1)
+            {
+                return null;
+            }
+            else
+            {
+                return orderDetails;
+            }
         }
     }
 }
